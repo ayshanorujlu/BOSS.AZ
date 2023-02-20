2f5094ebc39c1150c2893c753915e826229db3dd
user <user@WIN-2FA9I1VU4LK>
2023-02-20 16:27:00 +0400

    Add project files.

---
+++ BOSS___AZ/Program.cs
@@ -0,0 +1,12 @@
+﻿using BOSS___AZ;
+namespace BOSS.AZ
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Boss.az";
+            BossProgram.Start();
+        }
+    }
+}
